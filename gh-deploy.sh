7c0fe74f889ba0eae2f1166c64558f8351605472
Christopher De Jesus <christopher2653@gmail.com>
2025-12-29 16:40:20 -0500

    Pushing live site

--- gh-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# from https://stackoverflow.com/a/36782614
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
