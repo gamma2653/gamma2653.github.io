9a85a122b7884c1821fa4936e04ce87ad3fca052
Christopher De Jesus <christopher2653@gmail.com>
2026-01-05 15:42:27 -0500

    Fix banner for mozilla and safari

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -18,16 +18,21 @@ type BannerProps = {
   }
 }
 
-export default function Banner({img, imgDark, to, caption, text_overlay, classNames}: BannerProps) {
+export default function Banner({ img, imgDark, to, caption, text_overlay, classNames }: BannerProps) {
   return (
     <div className={clsx(classNames?.div)}>
-      <Link to={to} className={clsx("relative dark:text-white text-black hover:opacity-75", classNames?.div_a)}>
-        <img src={img} alt="Banner" className={clsx("object-cover w-full h-80 rounded-xl shadow-md block", imgDark ? "dark:hidden" : "", classNames?.div_a_img)}></img>
-        {imgDark && <img src={imgDark} alt="Banner Dark Mode" className={clsx("object-cover w-full h-80 rounded-xl shadow-md hidden dark:block mb-8", classNames?.div_a_imgDark)}></img>}
+      <Link to={to} className={clsx("relative dark:text-white text-black hover:opacity-75 no-underline", classNames?.div_a)}>
+        {!text_overlay &&
+          <img src={img} alt="Banner" className={clsx("object-cover w-full h-80 rounded-xl shadow-md block", imgDark ? "dark:hidden" : "", classNames?.div_a_img)}></img>
+        }
+        {!text_overlay && imgDark && <img src={imgDark} alt="Banner Dark Mode" className={clsx("object-cover w-full h-80 rounded-xl shadow-md hidden dark:block mb-8", classNames?.div_a_imgDark)}></img>}
         {caption && <p className={clsx("text-center mt-2", classNames?.div_a_caption)}>{caption}</p>}
-        {/* Overlay text on image */}
+        {/* Not using above image banner, use background image on head tag */}
         {text_overlay &&
-          <div className={clsx("absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2", classNames?.div_a_text_overlay)}>
+          <div
+            className={clsx("w-full h-80 rounded-xl shadow-md flex items-center justify-center bg-center bg-cover", classNames?.div_a_text_overlay)}
+            style={{ backgroundImage: `url(${img})` }}
+          >
             <h2 className="text-4xl font-bold drop-shadow-lg">{text_overlay}</h2>
           </div>
         }
